slowcoders/kotlin-nstw

[ABI Validation] Implemented ABI validation for JVM in KGP
Relates #KT-71168

libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/abi/AbiValidationJvmExtensionImpl.kt
@@ -1,0 +1,36 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.internal.abi
+
+import org.gradle.api.GradleException
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.TaskProvider
+import org.jetbrains.kotlin.gradle.dsl.abi.AbiFiltersSpec
+import org.jetbrains.kotlin.gradle.dsl.abi.AbiValidationJvmExtension
+import org.jetbrains.kotlin.gradle.tasks.abi.*
+import org.jetbrains.kotlin.gradle.tasks.abi.KotlinAbiCheckTask
+import org.jetbrains.kotlin.gradle.tasks.abi.KotlinAbiUpdateTask
+import org.jetbrains.kotlin.gradle.tasks.abi.KotlinJvmAbiDumpTask
+import org.jetbrains.kotlin.gradle.tasks.locateTask
+import org.jetbrains.kotlin.gradle.utils.newInstance
+import javax.inject.Inject
+
+internal abstract class AbiValidationJvmExtensionImpl @Inject constructor(private val project: Project) : AbiValidationJvmExtension {
+    override val filters: AbiFiltersSpec = project.objects.newInstance<AbiFiltersSpecImpl>(project.objects)
+
+    override val updateTaskProvider: TaskProvider<Task>
+        get() = project.locateTask(KotlinAbiUpdateTask.KOTLIN_JVM_TASK_NAME)
+            ?: throw GradleException("Couldn't locate task ${KotlinAbiUpdateTask.KOTLIN_JVM_TASK_NAME}")
+
+    override val dumpTaskProvider: TaskProvider<KotlinAbiDumpTask>
+        get() = project.locateTask(KotlinJvmAbiDumpTask.KOTLIN_JVM_TASK_NAME)
+            ?: throw GradleException("Couldn't locate task ${KotlinJvmAbiDumpTask.KOTLIN_JVM_TASK_NAME}")
+
+    override val checkTaskProvider: TaskProvider<KotlinAbiCheckTask>
+        get() = project.locateTask(KotlinJvmAbiCheckTask.KOTLIN_JVM_TASK_NAME)
+            ?: throw GradleException("Couldn't locate task ${KotlinJvmAbiCheckTask.KOTLIN_JVM_TASK_NAME}")
+}

libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/tasks/abi/KotlinAbiUpdateTask.kt
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.tasks.abi
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.tasks.*
+import org.gradle.work.DisableCachingByDefault
+
+@DisableCachingByDefault(because = "File copy should not be cacheable")
+internal abstract class KotlinAbiUpdateTask : DefaultTask() {
+    companion object {
+        const val KOTLIN_JVM_TASK_NAME = "updateAbi"
+    }
+
+    @get:OutputFile
+    abstract val referenceDump: RegularFileProperty
+
+    @get:InputFile
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val actualDump: RegularFileProperty
+
+    @TaskAction
+    internal fun overwrite() {
+        actualDump.get().asFile.copyTo(referenceDump.get().asFile, overwrite = true)
+    }
+}

Joanna2473/kotlin

libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/abi/AbiValidationJvmExtensionImpl.kt
@@ -1,0 +1,36 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.internal.abi
+
+import org.gradle.api.GradleException
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.TaskProvider
+import org.jetbrains.kotlin.gradle.dsl.abi.AbiFiltersSpec
+import org.jetbrains.kotlin.gradle.dsl.abi.AbiValidationJvmExtension
+import org.jetbrains.kotlin.gradle.tasks.abi.*
+import org.jetbrains.kotlin.gradle.tasks.abi.KotlinAbiCheckTask
+import org.jetbrains.kotlin.gradle.tasks.abi.KotlinAbiUpdateTask
+import org.jetbrains.kotlin.gradle.tasks.abi.KotlinJvmAbiDumpTask
+import org.jetbrains.kotlin.gradle.tasks.locateTask
+import org.jetbrains.kotlin.gradle.utils.newInstance
+import javax.inject.Inject
+
+internal abstract class AbiValidationJvmExtensionImpl @Inject constructor(private val project: Project) : AbiValidationJvmExtension {
+    override val filters: AbiFiltersSpec = project.objects.newInstance<AbiFiltersSpecImpl>(project.objects)
+
+    override val updateTaskProvider: TaskProvider<Task>
+        get() = project.locateTask(KotlinAbiUpdateTask.SIMPLE_TASK_NAME)
+            ?: throw GradleException("Couldn't locate task ${KotlinAbiUpdateTask.SIMPLE_TASK_NAME}")
+
+    override val dumpTaskProvider: TaskProvider<KotlinAbiDumpTask>
+        get() = project.locateTask(KotlinJvmAbiDumpTask.SIMPLE_TASK_NAME)
+            ?: throw GradleException("Couldn't locate task ${KotlinJvmAbiDumpTask.SIMPLE_TASK_NAME}")
+
+    override val checkTaskProvider: TaskProvider<KotlinAbiCheckTask>
+        get() = project.locateTask(KotlinAbiCheckTaskImpl.SIMPLE_TASK_NAME)
+            ?: throw GradleException("Couldn't locate task ${KotlinAbiCheckTaskImpl.SIMPLE_TASK_NAME}")
+}

libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/tasks/abi/KotlinAbiUpdateTask.kt
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.tasks.abi
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.tasks.*
+import org.gradle.work.DisableCachingByDefault
+
+@DisableCachingByDefault(because = "File copy should not be cacheable")
+internal abstract class KotlinAbiUpdateTask : DefaultTask() {
+    companion object {
+        const val SIMPLE_TASK_NAME = "updateAbi"
+        const val JVM_TASK_NAME = "updateJvmAbi"
+        const val KLIB_TASK_NAME = "updateKlibAbi"
+    }
+
+    @get:OutputFile
+    abstract val referenceDump: RegularFileProperty
+
+    @get:InputFile
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val actualDump: RegularFileProperty
+
+    @TaskAction
+    internal fun overwrite() {
+        actualDump.get().asFile.copyTo(referenceDump.get().asFile, overwrite = true)
+    }
+}