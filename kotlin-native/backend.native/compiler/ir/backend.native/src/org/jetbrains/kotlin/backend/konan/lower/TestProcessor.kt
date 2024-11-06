47b8b8cc729ab0159100d61132a1af5ca65dd881
Pavel Kunyavskiy <pavel.kunyavskiy@jetbrains.com>
2024-11-06 19:42:01 +0100

    [K/N] Support new nodes in test processor

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/lower/TestProcessor.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/lower/TestProcessor.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.backend.konan.lower
 
 import org.jetbrains.kotlin.backend.common.FileLoweringPass
 import org.jetbrains.kotlin.backend.common.ir.*
+import org.jetbrains.kotlin.backend.common.lower.UpgradeCallableReferences
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
 import org.jetbrains.kotlin.backend.common.reportWarning
 import org.jetbrains.kotlin.backend.konan.NativeGenerationState
@@ -21,8 +22,9 @@ import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.builders.*
 import org.jetbrains.kotlin.ir.builders.declarations.buildField
+import org.jetbrains.kotlin.ir.builders.declarations.buildFun
 import org.jetbrains.kotlin.ir.declarations.*
-import org.jetbrains.kotlin.ir.expressions.IrExpression
+import org.jetbrains.kotlin.ir.expressions.*
 import org.jetbrains.kotlin.ir.expressions.impl.*
 import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.ir.symbols.IrEnumEntrySymbol
@@ -81,11 +83,42 @@ internal class TestProcessor(private val generationState: NativeGenerationState)
         ignored: Boolean
     ) = add(TestFunction(function, kind, ignored))
 
+    fun IrFunction.toReference(parent: IrDeclarationParent) : IrRichFunctionReference {
+        val wrapper = factory.buildFun {
+            setSourceRange(this@toReference)
+            name = this@toReference.name
+            visibility = DescriptorVisibilities.LOCAL
+            returnType = this@toReference.returnType
+        }.apply {
+            this.parent = parent
+            copyValueParametersFrom(this@toReference)
+            val builder = context.createIrBuilder(symbol, startOffset, endOffset)
+            body = builder.irBlockBody {
+                +irReturn(irCall(this@toReference).apply {
+                    for ((index, param) in parameters.withIndex()) {
+                        arguments[index] = irGet(param)
+                    }
+                })
+            }
+        }
+        val referenceType = context.irBuiltIns.functionN(parameters.size).typeWith(parameters.map { it.type} + context.irBuiltIns.unitType)
+        return IrRichFunctionReferenceImpl(
+                startOffset = startOffset,
+                endOffset = endOffset,
+                type = referenceType,
+                reflectionTargetSymbol = symbol,
+                overriddenFunctionSymbol = UpgradeCallableReferences.selectSAMOverriddenFunction(referenceType),
+                invokeFunction = wrapper,
+                origin = IrStatementOrigin.LAMBDA,
+        )
+    }
+
     private fun <T : IrElement> IrStatementsBuilder<T>.generateFunctionRegistration(
             receiver: IrValueDeclaration,
             registerTestCase: IrFunction,
             registerFunction: IrFunction,
             functions: Collection<TestFunction>,
+            parent: IrDeclarationParent,
     ) {
         functions.forEach {
             if (it.kind == FunctionKind.TEST) {
@@ -93,13 +126,7 @@ internal class TestProcessor(private val generationState: NativeGenerationState)
                 +irCall(registerTestCase).apply {
                     dispatchReceiver = irGet(receiver)
                     putValueArgument(0, irString(it.functionName))
-                    putValueArgument(1, IrFunctionReferenceImpl(
-                            it.function.startOffset,
-                            it.function.endOffset,
-                            registerTestCase.valueParameters[1].type,
-                            it.function.symbol,
-                            typeArgumentsCount = 0,
-                            reflectionTarget = null))
+                    putValueArgument(1, it.function.toReference(parent))
                     putValueArgument(2, irBoolean(it.ignored))
                 }
             } else {
@@ -113,13 +140,7 @@ internal class TestProcessor(private val generationState: NativeGenerationState)
                             symbols.testFunctionKind.typeWithArguments(emptyList()),
                             testKindEntry)
                     )
-                    putValueArgument(1, IrFunctionReferenceImpl(
-                            it.function.startOffset,
-                            it.function.endOffset,
-                            registerFunction.valueParameters[1].type,
-                            it.function.symbol,
-                            typeArgumentsCount = 0,
-                            reflectionTarget = null))
+                    putValueArgument(1, it.function.toReference(parent))
                 }
             }
         }
@@ -456,8 +477,13 @@ internal class TestProcessor(private val generationState: NativeGenerationState)
                         putValueArgument(0, irString(suiteName))
                         putValueArgument(1, irBoolean(ignored))
                     }
-                    generateFunctionRegistration(testSuite.owner.thisReceiver!!,
-                            registerTestCase, registerFunction, functions)
+                    generateFunctionRegistration(
+                            testSuite.owner.thisReceiver!!,
+                            registerTestCase,
+                            registerFunction,
+                            functions,
+                            this@apply
+                    )
                 }
             }
 
@@ -573,7 +599,9 @@ internal class TestProcessor(private val generationState: NativeGenerationState)
                     testSuiteVal,
                     topLevelSuiteRegisterTestCase,
                     topLevelSuiteRegisterFunction,
-                    functions)
+                    functions,
+                    irFile,
+            )
         }
     }
 
