86de11ce6dd911072906ee99efe01278db0dc56f
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2024-11-22 13:02:08 +0100

    ~ K2 REPL: support classess - simple test works

--- plugins/scripting/scripting-tests/test/org/jetbrains/kotlin/scripting/test/repl/FirTestReplSnippetResolveExtensionImpl.kt
+++ plugins/scripting/scripting-tests/test/org/jetbrains/kotlin/scripting/test/repl/FirTestReplSnippetResolveExtensionImpl.kt
@@ -10,27 +10,16 @@ import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.copy
 import org.jetbrains.kotlin.fir.declarations.*
 import org.jetbrains.kotlin.fir.declarations.builder.buildPropertyCopy
-import org.jetbrains.kotlin.fir.declarations.builder.buildRegularClassCopy
-import org.jetbrains.kotlin.fir.declarations.builder.buildSimpleFunctionCopy
 import org.jetbrains.kotlin.fir.declarations.utils.originalReplSnippetSymbol
 import org.jetbrains.kotlin.fir.extensions.FirReplHistoryProvider
 import org.jetbrains.kotlin.fir.extensions.FirReplSnippetResolveExtension
 import org.jetbrains.kotlin.fir.extensions.replHistoryProvider
 import org.jetbrains.kotlin.fir.moduleData
 import org.jetbrains.kotlin.fir.scopes.FirScope
-import org.jetbrains.kotlin.scripting.resolve.FirReplHistoryScope
 import org.jetbrains.kotlin.fir.symbols.SymbolInternals
-import org.jetbrains.kotlin.fir.symbols.impl.FirClassLikeSymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirPropertySymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirReplSnippetSymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirVariableSymbol
-import org.jetbrains.kotlin.name.CallableId
-import org.jetbrains.kotlin.name.ClassId
-import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.fir.symbols.impl.*
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.name.NameUtils
+import org.jetbrains.kotlin.scripting.resolve.FirReplHistoryScope
 import kotlin.script.experimental.host.ScriptingHostConfiguration
 
 class FirTestReplSnippetResolveExtensionImpl(
@@ -52,11 +41,14 @@ class FirTestReplSnippetResolveExtensionImpl(
         replHistoryProvider.getSnippets().forEach { snippet ->
             if (currentSnippet == snippet) return@forEach
             snippet.fir.body.statements.forEach {
-                when (it) {
-                    is FirProperty -> properties.put(it.name, it.createCopyForState(snippet).symbol)
-                    is FirSimpleFunction -> functions.getOrPut(it.name, { ArrayList() }).add(it.createCopyForState(snippet).symbol)
-                    is FirRegularClass -> classLikes.put(it.name, it.createCopyForState(snippet).symbol)
-                    is FirTypeAlias -> classLikes.put(it.name, it.symbol)
+                if (it is FirDeclaration) {
+                    it.originalReplSnippetSymbol = snippet
+                    when (it) {
+                        is FirProperty -> properties.put(it.name, it.createCopyForState(snippet).symbol)
+                        is FirSimpleFunction -> functions.getOrPut(it.name, { ArrayList() }).add(it.symbol)
+                        is FirRegularClass -> classLikes.put(it.name, it.symbol)
+                        is FirTypeAlias -> classLikes.put(it.name, it.symbol)
+                    }
                 }
             }
         }
@@ -72,32 +64,6 @@ class FirTestReplSnippetResolveExtensionImpl(
             origin = FirDeclarationOrigin.FromOtherReplSnippet
             status = this@createCopyForState.status.copy(visibility = Visibilities.Local, isStatic = true)
             this.symbol = FirPropertySymbol(this@createCopyForState.symbol.callableId)
-        }.also {
-            it.originalReplSnippetSymbol = snippet
-        }
-    }
-
-    private fun FirReplSnippetSymbol.getTargetClassId(): ClassId {
-        // TODO: either make this transformation here but configure/retain target script name somewhere, or abstract it away, or make it on lowering
-        val snippetTargetName = NameUtils.getScriptNameForFile(name.asStringStripSpecialMarkers().removePrefix("script-"))
-        // TODO: take base package from snippet symbol (see todo elsewhere for adding it to the symbol)
-        return ClassId(FqName.ROOT, snippetTargetName)
-    }
-
-    private fun FirSimpleFunction.createCopyForState(snippet: FirReplSnippetSymbol): FirSimpleFunction {
-        return buildSimpleFunctionCopy(this) {
-            origin = FirDeclarationOrigin.FromOtherReplSnippet
-            status = this@createCopyForState.status.copy(visibility = Visibilities.Public, isStatic = true)
-            this.symbol = FirNamedFunctionSymbol(CallableId(snippet.getTargetClassId(), this@createCopyForState.symbol.callableId.callableName))
-        }.also {
-            it.originalReplSnippetSymbol = snippet
-        }
-    }
-
-    private fun FirRegularClass.createCopyForState(snippet: FirReplSnippetSymbol): FirRegularClass {
-        return buildRegularClassCopy(this) {
-            status = this@createCopyForState.status.copy(visibility = Visibilities.Public, isStatic = true)
-            this.symbol = FirRegularClassSymbol(snippet.getTargetClassId().createNestedClassId(name))
         }.also {
             it.originalReplSnippetSymbol = snippet
         }
