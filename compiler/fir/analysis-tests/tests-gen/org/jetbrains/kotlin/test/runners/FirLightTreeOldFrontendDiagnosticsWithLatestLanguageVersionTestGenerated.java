a9af85f5b902c5d3a959fdaea751ae178b659f73
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2024-12-03 17:15:33 +0100

    FIR: Warn about redundant name when function is not exposed
    Do not require name when not exposed.
    
     #KT-73466

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsWithLatestLanguageVersionTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsWithLatestLanguageVersionTestGenerated.java
@@ -41614,6 +41614,12 @@ public class FirLightTreeOldFrontendDiagnosticsWithLatestLanguageVersionTestGene
         }
 
         @Test
+        @TestMetadata("nonExposed.kt")
+        public void testNonExposed() {
+          runTest("compiler/testData/diagnostics/tests/valueClasses/jvmExposeBoxed/nonExposed.kt");
+        }
+
+        @Test
         @TestMetadata("simple.kt")
         public void testSimple() {
           runTest("compiler/testData/diagnostics/tests/valueClasses/jvmExposeBoxed/simple.kt");
