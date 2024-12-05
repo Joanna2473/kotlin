3a0c240f0a328bf968d18df684ad2fd42f574373
Leonid Startsev <leonid.startsev@jetbrains.com>
2024-12-05 17:02:12 +0100

    Add clarification for negative `times` parameter of `kotlin.repeat` function.
    #KT-54859 Fixed

--- libraries/stdlib/src/kotlin/util/Standard.kt
+++ libraries/stdlib/src/kotlin/util/Standard.kt
@@ -145,6 +145,8 @@ public inline fun <T> T.takeUnless(predicate: (T) -> Boolean): T? {
  *
  * A zero-based index of current iteration is passed as a parameter to [action].
  *
+ * If [times] parameter is negative or equal to zero, [action] is not invoked.
+ *
  * @sample samples.misc.ControlFlow.repeat
  */
 @kotlin.internal.InlineOnly
