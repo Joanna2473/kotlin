163a2fa71b501c9614d422e2148c97d6ac68c9f8
Aleksandra.Arsenteva <aleksandra.arsenteva@jetbrains.com>
2024-12-09 14:18:51 +0100

    [Tests] KQA-1566. Review fixes: added box tests, empty name usages, renaming

--- compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/TieredBackendJvmPsiTestGenerated.java
+++ compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/TieredBackendJvmPsiTestGenerated.java
@@ -23240,12 +23240,6 @@ public class TieredBackendJvmPsiTestGenerated extends AbstractTieredBackendJvmPs
           }
 
           @Test
-          @TestMetadata("emptyContextName.kt")
-          public void testEmptyContextName() {
-            runTest("compiler/fir/analysis-tests/testData/resolve/contextParameters/diagnostics/declarationAndUsages/emptyContextName.kt");
-          }
-
-          @Test
           @TestMetadata("inFunctionDeclaration.kt")
           public void testInFunctionDeclaration() {
             runTest("compiler/fir/analysis-tests/testData/resolve/contextParameters/diagnostics/declarationAndUsages/inFunctionDeclaration.kt");
