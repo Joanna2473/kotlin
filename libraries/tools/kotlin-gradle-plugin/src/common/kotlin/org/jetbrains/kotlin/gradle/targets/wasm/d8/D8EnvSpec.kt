217d6f001bb53672a3f473004964feff1b2f7934
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2024-12-10 16:24:12 +0100

    [Wasm] Small fix

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/wasm/d8/D8EnvSpec.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/wasm/d8/D8EnvSpec.kt
@@ -5,22 +5,14 @@
 
 package org.jetbrains.kotlin.gradle.targets.wasm.d8
 
-import org.gradle.api.Project
-import org.gradle.api.provider.Property
-import org.gradle.api.provider.Provider
-import org.gradle.api.tasks.TaskProvider
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.tasks.internal.CleanableStore
-import org.jetbrains.kotlin.gradle.utils.getFile
 
 /**
  * Spec for D8 - this target is available only for Wasm
  */
+@Suppress("DEPRECATION")
 @ExperimentalWasmDsl
 abstract class D8EnvSpec : org.jetbrains.kotlin.gradle.targets.js.d8.D8EnvSpec() {
-
-    val Project.d8SetupTaskProvider: TaskProvider<out D8SetupTask>
-        get() = project.tasks.withType(D8SetupTask::class.java).named(D8SetupTask.NAME)
 
     companion object {
         const val EXTENSION_NAME: String = "kotlinD8Spec"
